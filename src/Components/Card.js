850e398aea6fe100349b698f9bfc1148bc3f62f0
lioswin <lioswinrite@gmail.com>
2022-11-30 17:57:29 +0300

    added navbar

---
+++ src/Components/Card.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Card(){
+    return(
+        <div>
+        <div className="card">
+            <img src="https://source.unsplash.com/WLxQvbMyfas" alt="fuji"/>
+        </div>
+        <hr/>
+        </div>
+    )
+}
+
+export default Card;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Navbar(){
+    return (
+        <nav>
+            <h3>My travel journey.</h3>
+        </nav>
+    )
+}
+
+export default Navbar;
